c4a2183d73910c80ca96df09d99552cadc5739e7
Diana Garcia <dg38828@gmail.com>
2022-09-15 13:28:01 -0500

    Refactor log

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -32,16 +32,40 @@ function writeToLog(event, value, monsterHealth, playerHealth) {
     value: value,
     finalMonsterHealth: monsterHealth,
     finalPlayerHealth: playerHealth
+  };
+  switch (event) {
+    case LOG_EVENT_PLAYER_ATTACK:
+      logEntry.target = 'MONSTER';
+      break;
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry.target = 'MONSTER';
+      break;
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry.target = 'PLAYER';
+      break;
+    case LOG_EVENT_PLAYER_HEAL:
+      logEntry.target = 'PLAYER';
+      break;
+    case LOG_EVENT_GAME_OVER:
+      logEntry = {
+        event: event,
+        value: value,
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth
+      };
+      break;
+    default: 
+      logEntry = {};
   }
-  if (event === LOG_EVENT_PLAYER_ATTACK) {
-    logEntry.target = 'MONSTER';
-  } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-    logEntry.target = 'MONSTER';
-  } else if (event === LOG_EVENT_MONSTER_ATTACK) {
-    logEntry.target = 'PLAYER';
-  } else if (event === LOG_EVENT_PLAYER_HEAL) {
-    logEntry.target = 'PLAYER';
-  }
+  // if (event === LOG_EVENT_PLAYER_ATTACK) {
+  //   logEntry.target = 'MONSTER';
+  // } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+  //   logEntry.target = 'MONSTER';
+  // } else if (event === LOG_EVENT_MONSTER_ATTACK) {
+  //   logEntry.target = 'PLAYER';
+  // } else if (event === LOG_EVENT_PLAYER_HEAL) {
+  //   logEntry.target = 'PLAYER';
+  // }
   battleLog.push(logEntry);
 }
 
